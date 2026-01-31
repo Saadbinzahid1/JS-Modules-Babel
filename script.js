b9d2d339699dfa3eb5f0a9d733c7d6876f4cd07c
Saadbinzahid1 <sbinzahid2@gmail.com>
2026-01-31 20:29:43 +0500

    A little bit more practice

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 "use strict";
-import { addToCart } from "./shoppingCart.js";
-console.log("Importing Module");
-addToCart("Rice", 10);
+// import { addToCart } from "./shoppingCart.js";
+// console.log("Importing Module");
+// addToCart("Rice", 10);
+
+// import * as ShoppingCart from "./shoppingCart.js";
+// ShoppingCart.addToCart(ShoppingCart.favDish, ShoppingCart.price);
+// console.log(ShoppingCart.cart);
+
+import add from "./shoppingCart.js";
+add("Onion", 5);
--- shoppingCart.js
+++ shoppingCart.js
@@ -2,8 +2,14 @@
 
 console.log("Exporting Module");
 const cart = [];
+let favDish = "Biryani";
+let price = 10;
 export function addToCart(product, price) {
   cart.push({ product, price });
 }
 addToCart("Carrots", 25);
 console.log(cart);
+
+// export default function (product, price) {
+//   cart.push({ product, price });
+// }
