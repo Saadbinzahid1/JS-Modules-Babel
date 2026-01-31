4a59fb0c5625cc235e92bd1bf8f63aa7843c9aad
Saadbinzahid1 <sbinzahid2@gmail.com>
2026-01-31 20:50:44 +0500

    Tried async/await together with modules

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 "use strict";
-// import { addToCart } from "./shoppingCart.js";
-// console.log("Importing Module");
+import "./shoppingCart.js";
+
+console.log("Importing Module");
 // addToCart("Rice", 10);
 
 // import * as ShoppingCart from "./shoppingCart.js";
 // ShoppingCart.addToCart(ShoppingCart.favDish, ShoppingCart.price);
 // console.log(ShoppingCart.cart);
 
-import add from "./shoppingCart.js";
-add("Onion", 5);
+// import add from "./shoppingCart.js";
+// add("Onion", 5);
+
+console.log("Hi from script.js");
+async function getLastPost() {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const data = await res.json();
+  console.log(data);
+  return { title: data.at(-1).title, body: data.at(-1).body };
+}
+const lastPost = await getLastPost();
+console.log(lastPost);
--- shoppingCart.js
+++ shoppingCart.js
@@ -8,8 +8,12 @@ export function addToCart(product, price) {
   cart.push({ product, price });
 }
 addToCart("Carrots", 25);
-console.log(cart);
+// console.log(cart);
 
 // export default function (product, price) {
 //   cart.push({ product, price });
 // }
+
+console.log("Started fetching Todos");
+await fetch(`https://jsonplaceholder.typicode.com/todos`);
+console.log("Finished fetching Todos");
